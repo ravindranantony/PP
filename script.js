dcd8be156cb72af88a345846187986272a0fe6be
priya <svpriyaa2808@gmail.com>
2023-05-10 09:49:49 +0200

    adding workspace

--- script.js
+++
@@ -1,9 +0,0 @@
-var button = document.getElementById("coverletter");
-button.addEventListener("mouseover", function() {
-  this.style.cursor = "pointer";
-});
-
-var button = document.getElementById("resume");
-button.addEventListener("mouseover", function() {
-  this.style.cursor = "pointer";
-});
